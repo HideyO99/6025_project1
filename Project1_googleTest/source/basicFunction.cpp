e81fa4026dc5de6144b0db198494ec243caefa16
Siraphong <hideyo99@hotmail.com>
2022-09-27 21:06:20 -0400

    add function comment

--- Project1_googleTest/source/basicFunction.cpp
+++ Project1_googleTest/source/basicFunction.cpp
@@ -1,5 +1,17 @@
 #include "include/basicFunction.h"
 
+//////////////////////////////////////////////////////////
+//Summation												//
+// -----------------------------------------------------//
+// Calculate summation from begin value to end value	//
+// -----------------------------------------------------//
+// Input :												//
+// begin = start value									//
+// end = end value										//
+//														//
+// Output:												//
+// Sum value											//
+//////////////////////////////////////////////////////////
 int Sum(int begin, int end)
 {
 	int result = 0;
@@ -10,16 +22,55 @@ int Sum(int begin, int end)
 	return result;
 }
 
+//////////////////////////////////////////////////////////
+//Celsius to Fahrenheit									//
+// -----------------------------------------------------//
+// Convert Temperature in Degree Celsius to Fahranheit 	//
+// -----------------------------------------------------//
+// Input :												//
+// Tc = Temperature in Degree Celsius					//
+//														//
+// Output:												//
+// Temperature in Fahrenheit							//
+//////////////////////////////////////////////////////////
 float CelsiusToFahrenheit(float Tc)
 {
 	return (Tc*9/5)+32;
 }
 
+//////////////////////////////////////////////////////////
+//Fahrenheit to Celsius									//
+// -----------------------------------------------------//
+// Convert Temperature in Fahranheit to Degree Celsius	//
+// -----------------------------------------------------//
+// Input :												//
+// Tf = Temperature in Fahrenheit						//
+//														//
+// Output:												//
+// Temperature in Degree Celsius						//
+//////////////////////////////////////////////////////////
 float FahrenheitToCelsius(float Tf)
 {
 	return (Tf-32)*5/9;
 }
 
+//////////////////////////////////////////////////////////
+//Hysterasis											//
+// -----------------------------------------------------//
+// Toggle state to true if input is greather than high	//
+// threshold, and toggle to false if input lower than	//
+// threshold, but remain the current state if input in	//
+// between high and low threshold						//
+// -----------------------------------------------------//
+// Input :												//
+// HThreshold = high threshold							//
+// LThreshold = low threshold							//
+// Input = input value									//
+// CurState = current state								//
+//														//
+// Output:												//
+// State												//
+//////////////////////////////////////////////////////////
 bool Hysterasis(float HThreshold, float LThreshold, float Input, bool CurState)
 {
 	bool result = CurState;
@@ -39,6 +90,18 @@ bool Hysterasis(float HThreshold, float LThreshold, float Input, bool CurState)
 	return result;
 }
 
+//////////////////////////////////////////////////////////
+//Area of Triangle										//
+// -----------------------------------------------------//
+// Calculate area of triangle							//
+// -----------------------------------------------------//
+// Input :												//
+// Base = basis length									//
+// Height = height length								//
+//														//
+// Output:												//
+// Area of triangle										//
+//////////////////////////////////////////////////////////
 float AreaOfTriangle(float Base, float Height)
 {
 	return Base*Height/2;
--- Project1_googleTest/source/main.cpp
+++ Project1_googleTest/source/main.cpp
@@ -1,9 +1,27 @@
 #include <cmath>
 #include <iostream>
-
+#include "include/basicFunction.h"
 
 int main(int argc, char** argv)
 {
-	
+	int result = 0;
+	result = Sum(1, 10);
+	std::cout << "sum:" << result << std::endl;
+	float Fresult = 0.0f;
+	Fresult = CelsiusToFahrenheit(0);
+	std::cout << "CtoF:" << Fresult << std::endl;
+	Fresult = FahrenheitToCelsius(52);
+	std::cout << "FtoC:" << Fresult << std::endl;
+	bool state = false;
+	state = Hysterasis(5, 3, 4, state);
+	std::cout << "false " <<state<< std::endl;
+	state = Hysterasis(5, 3, 6, state);
+	std::cout << "high pass "<<state << std::endl;
+	state = Hysterasis(5, 3, 4, state);
+	std::cout << "b pass " << state << std::endl;
+	state = Hysterasis(5, 3, 2, state);
+	std::cout << "low pass " << state << std::endl;
+	Fresult = AreaOfTriangle(10, 5);
+	std::cout << "area:" << Fresult << std::endl;
 	return 0;
 }
